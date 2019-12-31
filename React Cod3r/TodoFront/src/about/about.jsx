4a2912a01000e0741de318cd4683d08aa5ecfd0d
Dionathan de Córdova <dionathan_bass@hotmail.com>
2019-12-30 23:51:15 -0300

    inicio projeto redux

--- React Cod3r/TodoFront/src/about/about.jsx
+++ React Cod3r/TodoFront/src/about/about.jsx
@@ -4,7 +4,7 @@ import PageHeader from '../template/pageHeader'
 export default props => (
     <div>
         <PageHeader name='About' small="Nós" />
-        <h2>Nossa história</h2>
+        <h2>Nossa históriasssssssss</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, velit. Sapiente accusamus asperiores sunt ipsam repudiandae quia dignissimos. Incidunt aliquam ad impedit molestiae perspiciatis magni nam minus dolor dolores. Provident.</p>
         <h2>Missão e visão</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias ducimus dolor nobis, fugit facilis, veniam officiis consectetur repellat architecto, quisquam magnam. Officia aperiam vitae exercitationem esse dolores amet delectus iusto.</p>
